9da29b64de2b5b14b94e502894c732f5600de25f
georgematos <georgemattos@gmail.com>
2020-08-13 15:39:36 -0300

    retornando lista estatica com o service e listando em tela

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -10,8 +10,19 @@ export class UsuarioService {
 
   public getUsuario(): Usuario {
     return new Usuario({
-      nome: "george", 
+      nome: "George", 
       email:'georgemattos@gmail.com',
     });
+  }
+
+  public getUsuarios(): Array<Usuario> {
+    return [
+      new Usuario({nome: "Laura", email: "laura@email.com"}),
+      new Usuario({nome: "Seth", email: "seth@email.com"}),
+      new Usuario({nome: "Birdie", email: "birdie@email.com"}),
+      new Usuario({nome: "Ryu", email: "ryu@email.com"}),
+      new Usuario({nome: "Juri", email: "juri@email.com"}),
+      new Usuario({nome: "Chun-li", email: "chunli@email.com"}),
+    ];
   }
 }
--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -10,12 +10,14 @@ import { UsuarioService } from '../services/usuario.service';
 export class UsuarioComponent implements OnInit {
 
   public usuario: Usuario;
+  public usuarios: Array<Usuario>;
   private usuarioService = new UsuarioService();
 
   constructor() { }
 
   ngOnInit(): void {
     this.usuario = this.usuarioService.getUsuario();
+    this.usuarios = this.usuarioService.getUsuarios();
     console.log(this.usuario);
   }
 
